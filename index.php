2969fb23127219d797cab308df6a9fece4ee7b3a
Hemel <hemel014@gmail.com>
2014-07-04 15:18:30 +0600

    Title changed in index.php page

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link type="text/css" rel="stylesheet" href="CSS/style.css" />
+<link type="text/css" rel="stylesheet" href="CSS/ui-lightness/jquery-ui-1.10.4.custom.css" />
+<title>Index Page</title>
+<script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript"  src="jquery-ui-1.10.4.custom.js" ></script>
+<script type="text/javascript" src="counter.js"></script>
+
+</head>
+
+<body>
+		
+       <span id="counter"></span>
+
+        
+</body>
+</html>
